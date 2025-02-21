4a22eed32fb188eaab4363e09ad105c21eeb4a68
Say <sailsman@tutanota.com>
2025-02-21 19:50:06 +0100

    properly integration test SetLobbySettingsController and adjust the controller structure for more concise error handling

--- backend/api/src/main/kotlin/software/shonk/lobby/adapters/incoming/setLobbySettings/SetLobbySettingsController.kt
+++ backend/api/src/main/kotlin/software/shonk/lobby/adapters/incoming/setLobbySettings/SetLobbySettingsController.kt
@@ -7,41 +7,62 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import kotlin.getValue
-import kotlin.text.toLongOrNull
 import org.koin.ktor.ext.inject
 import org.slf4j.LoggerFactory
 import software.shonk.lobby.adapters.incoming.addProgramToLobby.UNKNOWN_ERROR_MESSAGE
 import software.shonk.lobby.application.port.incoming.SetLobbySettingsUseCase
 import software.shonk.lobby.domain.InterpreterSettings
+import software.shonk.lobby.domain.exceptions.LobbyNotFoundException
 
 fun Route.configureSetLobbySettingsControllerV1() {
+
     val logger = LoggerFactory.getLogger("SetLobbySettingsControllerV1")
     val setLobbySettingsUseCase by inject<SetLobbySettingsUseCase>()
 
     post("/lobby/{lobbyId}/settings") {
-        val lobbyId =
-            call.parameters["lobbyId"]?.toLongOrNull()
-                ?: return@post call.respond(HttpStatusCode.BadRequest)
+        val incomingSettingsResult = runCatching { call.receive<InterpreterSettings>() }
+        incomingSettingsResult.onFailure {
+            logger.error("Unable to extract parameters from request...", it)
+            call.respond(HttpStatusCode.BadRequest, "Unable to parse settings from request")
+            return@post
+        }
+        val incomingSettings = incomingSettingsResult.getOrThrow()
 
-        val incomingSettings = call.receive<InterpreterSettings>()
-        val newSettings =
-            runCatching { SetLobbySettingsCommand(lobbyId, incomingSettings) }
-                .mapCatching { setLobbySettingsUseCase.setLobbySettings(it) }
+        val lobbyId = call.parameters["lobbyId"]
+        val constructSetLobbySettingsCommandResult = runCatching {
+            SetLobbySettingsCommand(lobbyId, incomingSettings)
+        }
 
-        newSettings.onFailure {
+        constructSetLobbySettingsCommandResult.onFailure {
             logger.error(
-                "Failed to set lobby settings, error on service layer after passing command!",
+                "Parameters for setLobbySettingsCommand construction failed basic validation...",
                 it,
             )
-            // todo change this to internal server error or at least re-evaluate
             call.respond(HttpStatusCode.BadRequest, it.message ?: UNKNOWN_ERROR_MESSAGE)
+            return@post
         }
-        newSettings.onSuccess { result ->
-            result.onFailure {
-                call.respond(HttpStatusCode.NotFound, it.message ?: UNKNOWN_ERROR_MESSAGE)
-            }
 
-            result.onSuccess { call.respond(HttpStatusCode.OK, "Settings updated") }
+        val didSetNewSettingsResult =
+            setLobbySettingsUseCase.setLobbySettings(
+                constructSetLobbySettingsCommandResult.getOrThrow()
+            )
+        didSetNewSettingsResult.onSuccess { call.respond(HttpStatusCode.OK, "Settings updated") }
+        didSetNewSettingsResult.onFailure {
+            when (it) {
+                is LobbyNotFoundException -> {
+                    logger.error("Failed to get settings from lobby, lobby does not exist")
+                    call.respond(HttpStatusCode.NotFound, it.message ?: UNKNOWN_ERROR_MESSAGE)
+                }
+                else -> {
+                    logger.error(
+                        "Failed to set lobby settings, unknown error on service layer after passing command!"
+                    )
+                    call.respond(
+                        HttpStatusCode.InternalServerError,
+                        it.message ?: UNKNOWN_ERROR_MESSAGE,
+                    )
+                }
+            }
         }
     }
 }
