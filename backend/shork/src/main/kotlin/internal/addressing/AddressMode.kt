a843f039b115c26e8c3dbbc7f03cdf2b1b4555aa
flamion <contact@flamion.dev>
2025-03-09 09:08:39 +0100

    [#368] Re-order AddressMode variants

--- backend/shork/src/main/kotlin/internal/addressing/AddressMode.kt
+++ backend/shork/src/main/kotlin/internal/addressing/AddressMode.kt
@@ -1,32 +1,32 @@
 package software.shonk.interpreter.internal.addressing
 
 internal enum class AddressMode {
-    // #
-    IMMEDIATE,
     // $
     DIRECT,
+    // #
+    IMMEDIATE,
     // *
     A_INDIRECT,
     // @
     B_INDIRECT,
     // {
     A_PRE_DECREMENT,
-    // }
-    A_POST_INCREMENT,
     // <
     B_PRE_DECREMENT,
+    // }
+    A_POST_INCREMENT,
     // >
     B_POST_INCREMENT;
 
     override fun toString(): String {
         return when (this) {
-            IMMEDIATE -> "# (Immediate)"
             DIRECT -> "$ (Direct)"
+            IMMEDIATE -> "# (Immediate)"
             A_INDIRECT -> "* (A Indirect)"
             B_INDIRECT -> "@ (B Indirect)"
             A_PRE_DECREMENT -> "{ (A Pre Decrement)"
-            A_POST_INCREMENT -> "} (A Post Increment)"
             B_PRE_DECREMENT -> "< (B Pre Decrement)"
+            A_POST_INCREMENT -> "} (A Post Increment)"
             B_POST_INCREMENT -> "> (B Post Increment)"
         }
     }
